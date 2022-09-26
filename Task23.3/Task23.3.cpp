6f708084d8466c5878c64dc8b5b6e0b1d8e4d9fd
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-26 09:57:35 +0300

    '26_09'

--- Task23.3/Task23.3.cpp
+++ Task23.3/Task23.3.cpp
@@ -1,29 +1,17 @@
 ﻿#include <iostream>
-#define PASS(name,result) name##_##result
-void passengers_car(int passengers[])
-{
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << "Enter the number of passengers in " << i + 1 << " car ";
-        std::cin >> passengers[i];
-        if (passengers[i] > 20)
-            std::cout << "there are more than 20 passengers in the car!!!!!\n";
-    }
-}
-int passengers_sum(int passengers[])
-{
-    int temp = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        temp += passengers[i];
-    }
-    return temp;
-}
+#define car(passengers,n) for (int i = 0; i < (n); i++)\
+    {std::cout << "Enter the number of passengers in " << i + 1 << " car ";\
+    std::cin >> passengers[i];}
+    
+#define sum(passengers,n) int temp = 0;\
+    for (int i = 0; i < (n); i++)\
+     temp += passengers[i];\
+     std::cout<<temp<<std::endl;
+
 int main()
 {
-    int passengers[10];
-    PASS(passengers,car)(passengers);
-    std::cout<<PASS(passengers, sum)(passengers)<<std::endl;
-    
-    
+    const int N = 10;
+    int passengers[N];
+    car(passengers,N);
+    sum(passengers,N);
 }
